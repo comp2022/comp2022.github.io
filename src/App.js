ce04885c8caa92babd2269dee1ab71ab08158cfa
Mitchell Merry <mitchellinminecraft@gmail.com>
2022-02-12 19:02:13 +1100

    Basic nav component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
+import Nav from './Nav'; // import the nav component
 
 function App() {
   return (
-    <div>
-      <h1>Hello from ALvin</h1>
-      <p>Testing again to see if i can approve my pull request</p>
-
-      <p>Pushing in develop branch</p>
-      <h3>Project List</h3>
-
-      <p>testing in feature_projectsubtitle</p>
-    </div>
+    <>
+      <Nav />
+    </>
   );
 }
 
