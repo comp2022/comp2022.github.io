3e19577760e51892dcad9e3b0164d1bcd30f5c81
mayP1211 <69944109+mayP1211@users.noreply.github.com>
2022-02-12 18:01:50 +1100

    update app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-
+    <div>
       <h1>Hello from ALvin</h1>
       <p>Testing again to see if i can approve my pull request</p>
       <p>Hello</p>
@@ -11,7 +11,7 @@ function App() {
       <h3>Project List</h3>
 
       <p>testing in feature_projectsubtitle</p>
-
+    </div>
   );
 }
 
