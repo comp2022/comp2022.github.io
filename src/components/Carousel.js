27d470bd1c39bba9bda6941f6c035acf21d30fca
Mitchell Merry <--global>
2022-02-15 21:27:04 +1100

    carousel functionality & title/subtitle with stroke

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,16 +1,22 @@
+import { useEffect, useState } from "react";
 import styles from "./Carousel.module.css";
+import * as classNames from 'classnames';
 
 export default function Carousel({ data }) {
+    const [activeSlide, setActiveSlide] = useState(0);
 
     return <div className={styles.carousel}>
         <ul>
-            { data.map(slide =>
-                <li className={styles.slide}>
+            { data.map((slide, slideIndex) => {
+                const classes = classNames(styles.slide, {[styles.active]: activeSlide === slideIndex})
+                return <li className={classes} key={slideIndex}>
                     <img src={slide.media} alt={slide.title} />
+                    <h1>{slide.title}</h1>
+                    <h3>{slide.subtitle}</h3>
                 </li>
-            )}
+            })}
         </ul>
-        <button className={styles.left_button}>&#8656;</button>
-        <button className={styles.right_button}>&#8658;</button>
+        <button className={styles.left_button} onClick={() => setActiveSlide((activeSlide+data.length-1) % data.length)}>&#8656;</button>
+        <button className={styles.right_button} onClick={() => setActiveSlide((activeSlide+1) % data.length)}>&#8658;</button>
     </div>
 }
