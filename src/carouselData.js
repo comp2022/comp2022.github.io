32f010e8ce6f5147c72cdbdbe3a4eb1a3e80ad73
Mitchell Merry <--global>
2022-02-15 23:25:24 +1100

    Basic carousel sliding animation functionality in place

--- src/carouselData.js
+++ src/carouselData.js
@@ -1,10 +1,11 @@
 import alg_vis from './media/alg_vis.png';
+import rocks from './media/rocks.png';
 
 export const data = Array.from({ length: 4 }).map((_, i) => (
   {
     "title": `Floodfill Visualiser ${i}`,
     "subtitle": `Simple visualiser for the floodfilling algorithm ${i}`,
-    "media": alg_vis
+    "media": (i % 2 === 0 ? alg_vis : rocks)
   }
 ));
   
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,11 +1,35 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./Carousel.module.css";
 import * as classNames from 'classnames';
 
 export default function Carousel({ data }) {
     const [activeSlide, setActiveSlide] = useState(0);
+    const carouselParent = useRef();
 
-    return <div className={styles.carousel}>
+    const changeSlide = (offset) => {
+        if(!carouselParent) return;
+        
+        const slides = carouselParent.current.querySelectorAll(`ul > .${styles.slide}`);
+        
+        // deal with left/right shenanigans
+        const [prevAnim, nextAnim] = offset > 0 ? [styles.activeLeftPrev, styles.activeLeftNext]
+                                        : [styles.activeRightPrev, styles.activeRightNext];
+
+        // get the index of the slide we're changing to
+        const nextSlide = (activeSlide + data.length + offset) % data.length;
+        slides[activeSlide].classList.add(prevAnim);
+        slides[nextSlide].classList.add(nextAnim);
+
+        // remove animations after transition is finished
+        setTimeout(() => {
+            slides[activeSlide].classList.remove(prevAnim);
+            slides[nextSlide].classList.remove(nextAnim);
+
+            setActiveSlide(nextSlide);
+        }, 1000);
+    }
+
+    return <div className={styles.carousel} ref={carouselParent}>
         <ul>
             { data.map((slide, slideIndex) => {
                 const classes = classNames(styles.slide, {[styles.active]: activeSlide === slideIndex})
@@ -16,7 +40,7 @@ export default function Carousel({ data }) {
                 </li>
             })}
         </ul>
-        <button className={styles.left_button} onClick={() => setActiveSlide((activeSlide+data.length-1) % data.length)}>&#8656;</button>
-        <button className={styles.right_button} onClick={() => setActiveSlide((activeSlide+1) % data.length)}>&#8658;</button>
+        <button className={styles.left_button}  onClick={() => changeSlide(-1)}>&#8656;</button>
+        <button className={styles.right_button} onClick={() => changeSlide(+1)}>&#8658;</button>
     </div>
 }
