f4c8c0f8097234ffd5ecb743a0089eb4c92b7ff7
mayphyuoo <mayphyuoo1211@gmail.com>
2022-02-17 01:23:44 +1100

    create App.js

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../index.css';
+import { projects } from '../project-data';
+import Nav from './Nav'; // import the nav component
+import CardGroup from './CardGroup';
+
+function App() {
+    return (
+        // shorthand for the React.Fragment
+        <>
+            <Nav />
+            <h1>COMP2022</h1>
+            <div id="container">
+                <CardGroup cards={projects} />
+            </div>
+        </>
+    );
+}
+
+export default App;
